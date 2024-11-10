311bb629349ce02d3b790db92013605a7714e0b3
Samion Suwito <79575349+samionsuwito@users.noreply.github.com>
2024-11-10 12:38:01 -0800

    revert version

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,9 @@ package «lean4-example» {
   -- add package configuration options here
 }
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_lib «Lean4Example» {
   -- add library configuration options here
